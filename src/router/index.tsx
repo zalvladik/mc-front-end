a90566f97585f63b64009e3adfe7b8a259f80470
VladislavZ <zalipkav@ukr.net>
2024-07-18 17:26:54 +0300

    master

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,17 @@
 import { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-// import AuctionLayout from 'src/layouts/AuctionLayout'
-// import AuthLayout from 'src/layouts/AuthLayout'
+import AuctionLayout from 'src/layouts/AuctionLayout'
+import AuthLayout from 'src/layouts/AuthLayout'
 import NavLayout from 'src/layouts/NavLayouts'
 import { RoutesPath } from 'src/router/routes'
 
 const ErrorPage = lazy(() => import('src/pages/ErrorPage'))
-const HomePage = lazy(() => import('src/pages/HomePage'))
+// const HomePage = lazy(() => import('src/pages/HomePage'))
 const RulePage = lazy(() => import('src/pages/RulePage'))
-// const WikiPage = lazy(() => import('src/pages/WikiPage'))
-// const SignInPage = lazy(() => import('src/pages/SignInPage'))
-// const InventoryPage = lazy(() => import('src/pages/InventoryPage'))
-// const ProfilePage = lazy(() => import('src/pages/ProfilePage'))
+const WikiPage = lazy(() => import('src/pages/WikiPage'))
+const SignInPage = lazy(() => import('src/pages/SignInPage'))
+const InventoryPage = lazy(() => import('src/pages/InventoryPage'))
+const ProfilePage = lazy(() => import('src/pages/ProfilePage'))
 
 export const Router = createBrowserRouter([
   {
@@ -19,22 +19,22 @@ export const Router = createBrowserRouter([
     element: <NavLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: RoutesPath.HOME, element: <HomePage /> },
+      // { path: RoutesPath.HOME, element: <HomePage /> },
       { path: RoutesPath.RULES, element: <RulePage /> },
-      // { path: RoutesPath.SIGN_IN, element: <SignInPage /> },
-      // {
-      //   path: RoutesPath.USER,
-      //   element: <AuthLayout />,
-      //   children: [
-      //     { path: RoutesPath.PROFILE, element: <ProfilePage /> },
-      //     { path: RoutesPath.INVENTORY, element: <InventoryPage /> },
-      //     {
-      //       path: RoutesPath.AUCTION,
-      //       element: <AuctionLayout />,
-      //     },
-      //     { path: RoutesPath.WIKI, element: <WikiPage /> },
-      //   ],
-      // },
+      { path: RoutesPath.SIGN_IN, element: <SignInPage /> },
+      {
+        path: RoutesPath.USER,
+        element: <AuthLayout />,
+        children: [
+          { path: RoutesPath.PROFILE, element: <ProfilePage /> },
+          { path: RoutesPath.INVENTORY, element: <InventoryPage /> },
+          {
+            path: RoutesPath.AUCTION,
+            element: <AuctionLayout />,
+          },
+          { path: RoutesPath.WIKI, element: <WikiPage /> },
+        ],
+      },
     ],
   },
 ])
