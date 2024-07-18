1a17877785a1d0175bc79bcc961ba2fabb802ecb
VladislavZ <zalipkav@ukr.net>
2024-07-18 17:28:52 +0300

    master

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,7 +6,7 @@ import NavLayout from 'src/layouts/NavLayouts'
 import { RoutesPath } from 'src/router/routes'
 
 const ErrorPage = lazy(() => import('src/pages/ErrorPage'))
-// const HomePage = lazy(() => import('src/pages/HomePage'))
+const HomePage = lazy(() => import('src/pages/HomePage'))
 const RulePage = lazy(() => import('src/pages/RulePage'))
 const WikiPage = lazy(() => import('src/pages/WikiPage'))
 const SignInPage = lazy(() => import('src/pages/SignInPage'))
@@ -19,7 +19,7 @@ export const Router = createBrowserRouter([
     element: <NavLayout />,
     errorElement: <ErrorPage />,
     children: [
-      // { path: RoutesPath.HOME, element: <HomePage /> },
+      { path: RoutesPath.HOME, element: <HomePage /> },
       { path: RoutesPath.RULES, element: <RulePage /> },
       { path: RoutesPath.SIGN_IN, element: <SignInPage /> },
       {
