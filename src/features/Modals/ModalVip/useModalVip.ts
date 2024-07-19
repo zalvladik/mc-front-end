7f18980ad3c5220799e89f357d4c795732591c69
VladislavZ <zalipkav@ukr.net>
2024-07-19 17:59:43 +0300

    created modal for bye VIP

--- src/features/Modals/ModalVip/useModalVip.ts
+++ src/features/Modals/ModalVip/useModalVip.ts
@@ -1,17 +1,10 @@
-import { useEffect } from 'react'
 import { useByeVip } from 'src/hooks/useByeVip'
 import type { VipEnum } from 'src/types'
 
 import type { UseModalVipProps } from 'src/features/Modals/ModalVip/types'
 
 export const useModalVip = ({ afterSubmit }: UseModalVipProps) => {
-  const { data, isLoading, isSuccess, mutate } = useByeVip()
-
-  useEffect(() => {
-    if (data && isSuccess) {
-      afterSubmit(data)
-    }
-  }, [data, isLoading, isSuccess])
+  const { isLoading, mutate } = useByeVip(afterSubmit)
 
   const byeVip = (vip: VipEnum) => {
     mutate(vip)
--- src/hooks/useByeVip/index.ts
+++ src/hooks/useByeVip/index.ts
@@ -1,18 +1,21 @@
 import { useMutation } from 'react-query'
 import { useModals } from 'src/contexts/ModalProvider/useModals'
 import { useToast } from 'src/contexts/ToastProvider/useToast'
+import type { UserT } from 'src/contexts/UserProvider/types'
 import Vip from 'src/services/api/Vip'
 import type { VipEnum } from 'src/types'
 
-export const useByeVip = () => {
+export const useByeVip = (afterSubmit: (value: UserT) => void) => {
   const { onClose } = useModals()
 
   const toast = useToast()
 
-  const { data, mutate, isLoading, isSuccess } = useMutation({
+  const { data, mutate, isLoading } = useMutation({
     mutationFn: (vip: VipEnum) => Vip.byeVip(vip),
-    onSuccess: () => {
+    onSuccess: data => {
       toast.success({ message: ['Vip успішно куплено !'] })
+
+      afterSubmit(data)
       onClose()
     },
     onError: (error: Error) => {
@@ -20,5 +23,5 @@ export const useByeVip = () => {
     },
   })
 
-  return { mutate, data, isLoading, isSuccess }
+  return { mutate, data, isLoading }
 }
